95620410779b61fb5e1998d711713f8db40e2e42
PuzzleLeaf <cmtyx2@naver.com>
2017-05-14 16:05:34 +0900

    소스트리 테스트

--- app/src/main/java/puzzleleaf/tistory/com/teamclinic/Story/Story.java
+++ app/src/main/java/puzzleleaf/tistory/com/teamclinic/Story/Story.java
@@ -16,7 +16,7 @@ import puzzleleaf.tistory.com.teamclinic.processing.ReviewTree;
 
 public class Story extends AppCompatActivity {
 
-
+//소스트리 테스트
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
